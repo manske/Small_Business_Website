5072601f33bafa4943e0bfd2240c4b1a98bb9fef
Manske <Manske@Nates-MacBook-Pro-3.local>
2014-07-29 20:01:52 -0500

    New Changes

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 $(document).ready(function() {
 	$("#thex").click(function() {
+		$( "#thex" ).toggleClass( "rotate" );
+		$( "#home" ).toggleClass( "show" )
+
+	});
+	$("#home").click(function() {
   		if ( $( ".thingtohide:first" ).is( ":hidden" ) ) {
     		$( ".thingtohide" ).show();
-  } 	else {
+ 	 	} 
+ 	 	else {
     		$( ".thingtohide" ).hide();
-  }
-  		
-});
+ 	 	}	
+  });
 });
